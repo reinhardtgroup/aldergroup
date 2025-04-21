4826dfa703a0ce2f2a11c57fd318821cb91d8b89
joeinator2 <joseph@ridgewaybrief.org>
2025-04-20 22:47:35 -0500

    polling

--- polling/script.js
+++ polling/script.js
@@ -1,26 +1,26 @@
 function loadArticles(section = null) {
-    fetch('data/articles.json')
+    fetch('../data/polls.json')
       .then(res => res.json())
       .then(articles => {
-        const list = document.getElementById('article-list');
+        const list = document.getElementById('polling-list');
         const filtered = section
           ? articles.filter(a => a.section === section)
           : articles;
   
         filtered.forEach(article => {
           const li = document.createElement('li');
-          li.innerHTML = `<a href="articles/${article.slug}.html">${article.title}</a> 
+          li.innerHTML = `<a href="polling/${article.slug}.html">${article.title}</a> 
             <small>(${article.date})</small><br>
             <span>${article.preview}</span>`;
           list.appendChild(li);
         });
       })
       .catch(err => {
-        console.error("Error loading articles:", err);
+        console.error("Error loading polls:", err);
       });
   }
   
   // Auto-load on homepage
-  if (document.getElementById('article-list') && !window.location.pathname.includes('/sections/')) {
+  if (document.getElementById('polling-list')) {
     loadArticles();
   }
